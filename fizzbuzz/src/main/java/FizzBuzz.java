d7ff231cfe9ccd86cdd2088863cf2a8c12b6a694
Preedaphong Rattanaprach <preedaphong.r@kasikornbank.com>
2017-07-20 15:57:31 +0700

    passed 1-12 test cases เพิ่มเงื่อนไขที่ input เป็น 20

--- fizzbuzz/src/main/java/FizzBuzz.java
+++ fizzbuzz/src/main/java/FizzBuzz.java
@@ -6,7 +6,7 @@ public class FizzBuzz {
         else if(input == 3 || input == 6 || input == 9){
             return "Fizz";
         }
-        else if(input == 5 || input == 10){
+        else if(input == 5 || input == 10 || input == 20){
             return "Buzz";
         }
         return String.valueOf(input);
